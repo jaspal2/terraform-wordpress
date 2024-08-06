cf46e5a0129eccdad6b1ea73e98c03868ab4fa31
jaspal singh <jaspal.singh@monash.edu>
2024-08-06 15:24:36 +1000

    Adding correct AZs

--- main.tf
+++ main.tf
@@ -10,6 +10,7 @@ module "vpc" {
   name = "my-vpc"
   cidr = "10.0.0.0/24"
 
+  azs             = ["ap-southeast-2a", "ap-southeast-2b", "ap-southeast-2c"]
   private_subnets = ["10.0.1.0/24", "10.0.2.0/24", "10.0.3.0/24"]
   public_subnets  = ["10.0.101.0/24", "10.0.102.0/24", "10.0.103.0/24"]
   database_subnets = ["10.0.0.0/28", "10.0.0.16/28", "10.0.0.32/28"]
