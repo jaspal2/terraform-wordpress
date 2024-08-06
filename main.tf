d71b814e8cd45820afa1eebc1511ff32a3c4d648
jaspal singh <jaspal.singh@monash.edu>
2024-08-06 15:44:55 +1000

    Adding correct AZs

--- main.tf
+++ main.tf
@@ -11,8 +11,8 @@ module "vpc" {
   cidr = "10.0.0.0/24"
 
   azs             = ["ap-southeast-2a", "ap-southeast-2b", "ap-southeast-2c"]
-  private_subnets = ["10.0.1.0/24", "10.0.2.0/24", "10.0.3.0/24"]
-  public_subnets  = ["10.0.101.0/24", "10.0.102.0/24", "10.0.103.0/24"]
+  private_subnets = ["10.0.0.48/28", "10.0.0.64/28", "10.0.0.80/28"]
+  public_subnets  = ["10.0.0.96/28", "10.0.0.112/28", "10.0.0.128/28"]
   database_subnets = ["10.0.0.0/28", "10.0.0.16/28", "10.0.0.32/28"]
 
   enable_nat_gateway = true
