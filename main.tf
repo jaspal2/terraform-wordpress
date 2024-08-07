e42716933d3d0ec50c153c1942bfc4de72af0aad
jaspal singh <jaspal.singh@monash.edu>
2024-08-07 11:14:21 +1000

    Changed security group module

--- main.tf
+++ main.tf
@@ -4,54 +4,13 @@
 
 
 
-module "vp" {
+module "vpc" {
   source = "./vpc_module"
 
 }
 
-
-module "db_sg" {
-  source = "terraform-aws-modules/security-group/aws"
-
-  name        = "db-sg"
-  description = "Security group for RDS database"
-  vpc_id      = module.vpc.vpc_id
-
-  ingress_cidr_blocks      = ["0.0.0.0/0"]
-  ingress_rules            = ["https-443-tcp"]
-  ingress_with_cidr_blocks = [
-    {
-      from_port   = 0
-      to_port     =65535
-      protocol    = "tcp"
-      description = "All tcp"
-      cidr_blocks = "10.10.0.0/16"
-    }
-  ]
-}
-
-
-module "public_sg" {
-  source = "terraform-aws-modules/security-group/aws"
-
-  name        = "user-service"
-  description = "Security group for user-service with custom ports open within VPC, and PostgreSQL publicly open"
-  vpc_id      = module.vpc.vpc_id
-
-
-  ingress_with_cidr_blocks = [
-    {
-      from_port   = 8080
-      to_port     = 8090
-      protocol    = "tcp"
-      description = "User-service ports"
-      cidr_blocks = "10.10.0.0/16"
-    },
-    {
-      rule        = "postgresql-tcp"
-      cidr_blocks = "0.0.0.0/0"
-    },
-  ]
+module "security_group" {
+  source  = "./security_group_module"
 }
 
 
